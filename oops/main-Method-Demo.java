a9ea9b492344ca6b3eca64b2d78c3c3142d02a50
singh <107493787+u4user0@users.noreply.github.com>
2022-07-30 20:01:09 +0530

    Create main-Method-Demo.java

---
+++ oops/main-Method-Demo.java
@@ -0,0 +1,22 @@
+class A {
+	public static void main ( String ...args ) {
+		System.out.println("Main method of class: A called");
+	}
+}
+class B {
+	public static void main ( String[] args ) {
+		System.out.println("Main method of class: B called");
+	}
+}
+public class mainMethodDemo {
+
+	public static void main ( String[] args ) {
+		System.out.println("Diffrent ways to call main method");
+		B.main(null);
+		B.main(args);
+		String[] array={"hey"};
+		B.main(array);//by array
+		A.main();//but variable length args is important (...)
+	}
+
+}
