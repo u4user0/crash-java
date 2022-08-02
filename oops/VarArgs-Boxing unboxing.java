af5c71ef653dad25d6ef804b037a48738e82d733
singh <107493787+u4user0@users.noreply.github.com>
2022-08-02 14:37:08 +0530

    Create VarArgs-Boxing unboxing.java

---
+++ oops/VarArgs-Boxing unboxing.java
@@ -0,0 +1,23 @@
+public class overloadingVarArgsBoxingUnboxingEx {
+	static void method(String a) {
+		System.out.println("First name: "+a);
+	}
+	static void method(String a, String b) {
+		System.out.println("First & middle name"+a+ " "+b);
+	}
+	static void method(String a, String b, String c) {
+		System.out.println("Full name: "+ a + " "+b+" "+c);
+	}
+	static void show(String ... size) {
+		for(String i : size)
+		   System.out.print("Name: "+ i);
+	}
+
+	public static void main ( String[] args ) {
+		method("umberala");
+		method("umnerela","used","in rain");
+		method("charged","atom");
+		show("a4apple","b4boll","c4cat","d4dog","e4egg");
+	}
+
+}
